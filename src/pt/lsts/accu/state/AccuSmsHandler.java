08e4791c2d8a0808e980ca8cc7c34cc5ba3b3209
José Pinto <zepinto@gmail.com>
2014-02-27 14:06:45 +0000

    Fixed toast creation in AccuSmsHandler.

--- src/pt/lsts/accu/state/AccuSmsHandler.java
+++ src/pt/lsts/accu/state/AccuSmsHandler.java
@@ -4,6 +4,7 @@ import pt.lsts.accu.msg.IMCManager;
 import pt.lsts.accu.msg.IMCSubscriber;
 import pt.lsts.imc.IMCMessage;
 import pt.lsts.imc.Sms;
+import android.content.Context;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.Toast;
@@ -11,19 +12,24 @@ import android.widget.Toast;
 public class AccuSmsHandler implements IMCSubscriber {
 
 	private IMCManager mManager;
+	private Context mContext;
 	
 	@Override
 	public void onReceive(IMCMessage msg) {
+		
 		if (msg.getMgid() == Sms.ID_STATIC && msg.getDst() == mManager.getLocalId()) {
 			Log.i("SmsManager", "Sending an SMS to "+msg.getString("number"));
 			sendSms(msg.getString("number"), msg.getString("contents"), msg.getInteger("timeout"));
 		}
 		else {
 			Log.w("SmsManager", "Ignoring Sms request");
+			System.out.println(mManager.getLocalId());
+			System.out.println(msg.toString());
 		}
 	}
 	
-	public AccuSmsHandler(IMCManager imcComms) {
+	public AccuSmsHandler(Context context, IMCManager imcComms) {
+		mContext = context;
 		mManager = imcComms;
 		mManager.addSubscriber(this, "Sms");
 	}
@@ -31,7 +37,7 @@ public class AccuSmsHandler implements IMCSubscriber {
 	private void sendSms(String destination, String text, int timeout) {
 		SmsManager sms = SmsManager.getDefault();
 		sms.sendTextMessage(destination, null, text, null, null);
-		Toast.makeText(null, "SMS sent to "+destination, Toast.LENGTH_LONG).show();
+		Toast.makeText(mContext, "SMS sent to "+destination, Toast.LENGTH_LONG).show();
 	}
 	
 	public void stop() {
