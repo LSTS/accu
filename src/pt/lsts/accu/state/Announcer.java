e4089e1ef91e16cb8adc86719f9511050efe8478
Paulo Dias <paulo.sousa.dias@gmail.com>
2014-06-11 10:43:12 +0100

    Update Announcer to send the ACCU location.

--- src/pt/lsts/accu/state/Announcer.java
+++ src/pt/lsts/accu/state/Announcer.java
@@ -16,25 +16,50 @@ public class Announcer
 	public static final long ANNOUNCE_DELAY = 10000; // 10 seconds
 	public static final boolean DEBUG = false;
 	
-	IMCMessage announce;
-	IMCManager imm;
-	String broadcastAddress;
-	String multicastAddress;
-	AccuTimer at;
+	private IMCMessage announce;
+	private IMCManager imm;
+	private String broadcastAddress;
+	private String multicastAddress;
+	private AccuTimer at;
+	private int gpsAddListenerCounter = 0;
+	private GPSManager gpsManager; // = Accu.getInstance().getGpsManager();
 	
-	Runnable task = new Runnable()
+	private Runnable task = new Runnable()
 	{
-
 		@Override
-		public void run() 
+		public void run()
 		{
 			if(DEBUG)Log.i(TAG,announce.toString());
-			if(imm.getListener()==null) return;
+			if(imm.getListener()==null)
+			{
+				gpsManager.removeListener(locListener);
+				gpsAddListenerCounter = 0;
+				return;
+			}
+			if (gpsAddListenerCounter == 0) 
+			{
+				gpsManager.addListener(locListener);
+			}
+			else if (gpsAddListenerCounter == 2) 
+			{
+				gpsManager.removeListener(locListener);
+			}
+			gpsAddListenerCounter = (gpsAddListenerCounter + 1) % 4;
+			
+			updateLocationOnAnnounce();
 			for(int i = 0; i <5;i++)
 			{
 				imm.send(broadcastAddress,ANNOUNCE_PORT+i, announce);
 				imm.send(multicastAddress,ANNOUNCE_PORT+i, announce);
 			}
+		}
+	};
+	private String services = "";
+	private LocationChangeListener locListener = new LocationChangeListener() {
+		@Override
+		public void onLocationChange(Location location) 
+		{
+			updateLocationOnAnnounce();
 		}
 	};
 	
@@ -50,25 +75,44 @@ public class Announcer
 
 	public void start()
 	{
+		gpsManager = Accu.getInstance().getGpsManager();
+		
 		generateAnnounce(); // Generate announce message only on start
 		at.start();
+		
+		gpsManager.addListener(locListener);
 	}
+	
 	public void stop()
 	{
 		at.stop();
+		gpsManager.removeListener(locListener);
 	}
 	
 	public void setBroadcastAddress(String addr)
 	{
 		broadcastAddress = addr; 
 	}
+	
+	private void updateLocationOnAnnounce()
+	{
+		Location currentLocation = Accu.getInstance().getGpsManager().getCurrentLocation();
+		double lat = currentLocation.getLatitude();
+		double lon = currentLocation.getLongitude();
+		double height = currentLocation.getAltitude();
+		
+		announce.setValue("lat", Math.toRadians(lat));
+		announce.setValue("lon", Math.toRadians(lon));
+		announce.setValue("height", height);
+	}
+	
 	/**
 	 * This function can be called to (re)generate the Announce message sent by the console
 	 */
 	public void generateAnnounce()
 	{
 		String ipfull = MUtil.getLocalIpAddress();
-		Location currentLocation = Accu.getInstance().getGpsManager().getCurrentLocation();
+//		Location currentLocation = Accu.getInstance().getGpsManager().getCurrentLocation();
 		
 		String[] ip = new String[4];
 		
@@ -83,26 +127,28 @@ public class Announcer
 		
 		String sysName = "accu-"+ip[2]+ip[3]; 
 		String sysType = "CCU"; 
-		double lat = currentLocation.getLatitude();
-		double lon = currentLocation.getLatitude();
-		double height = currentLocation.getAltitude();
+//		double lat = currentLocation.getLatitude();
+//		double lon = currentLocation.getLongitude();
+//		double height = currentLocation.getAltitude();
 		
 		int owner = 0xFFFF;
-		String services = "imc+udp://"+MUtil.getLocalIpAddress()+":6001/;imc+udp://"+MUtil.getLocalIpAddress()+":6001/sms"; //FIXME
+		services = "imc+udp://"+MUtil.getLocalIpAddress()+":6001/;imc+udp://"+MUtil.getLocalIpAddress()+":6001/sms"; //FIXME
 		try {
 			announce = IMCDefinition.getInstance().create("Announce", 
-					"sys_name",sysName,
-					"sys_type",sysType,
+					"sys_name", sysName,
+					"sys_type", sysType,
 					"owner",owner,
-					"lat",lat,
-					"lon",lon,
-					"height",height,
-					"services",services);
+//					"lat", Math.toRadians(lat),
+//					"lon", Math.toRadians(lon),
+//					"height", height,
+					"services", services);
+			updateLocationOnAnnounce();
 			announce.getHeader().setValue("src", imm.getLocalId());
 			announce.getHeader().setValue("src_ent", 255);
 			announce.getHeader().setValue("dst_ent", 255);
-			
-		} catch (Exception e) {
+		}
+		catch (Exception e) 
+		{
 			e.printStackTrace();
 		}
 	}
